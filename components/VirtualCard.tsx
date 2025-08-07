0b2eec84930c40628bba6fb172fb2008ebb0c8fd
appslms <tbnalfaro@gmail.com>
2025-08-07 01:14:46 -0600

    cards

--- components/VirtualCard.tsx
+++ components/VirtualCard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useCardRotation } from '../hooks/useCardRotation';
 import { MAX_ROTATION_X, MAX_ROTATION_Y } from '../constants';
 
-const cardWidth = 350;
-const cardHeight = 600;
+const cardWidth = 420;
+const cardHeight = 520;
 
 export const VirtualCard = () => {
   const { rotation, handleMouseMove, handleMouseLeave, requestDeviceOrientationPermission, isGyroActive } = useCardRotation();
